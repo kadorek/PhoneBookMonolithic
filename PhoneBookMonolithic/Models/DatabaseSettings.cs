7eb00272f8c383422c99914d4f0c82883206a0c6
KadoreK <haltaskadir@gmail.com>
2022-06-05 16:06:32 +0300

    DB bağlantı ayarları yapıldı

---
+++ PhoneBookMonolithic/Models/DatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace PhoneBookMonolithic.Models
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null;
+        public string DatabaseName { get; set; } = null;
+        public string ContactsCollectionName { get; set; } = null;
+        public string ReportsCollectionName { get; set; } = null;
+    }
+}
