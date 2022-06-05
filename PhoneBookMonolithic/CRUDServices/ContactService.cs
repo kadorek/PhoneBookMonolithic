4988190a663d5337052b65713f2d87b5ddd1ac15
KadoreK <haltaskadir@gmail.com>
2022-06-05 17:00:24 +0300

    Contact modeli için service eklendi

---
+++ PhoneBookMonolithic/CRUDServices/ContactService.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using PhoneBookMonolithic.Models;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+
+
+
+namespace PhoneBookMonolithic.CRUDServices
+{
+    public class ContactService
+    {
+        private readonly IMongoCollection<Contact> _contactCollection;
+
+        public ContactService(IOptions<DatabaseSettings> dbSettings)
+        {
+            var mClient = new MongoClient(dbSettings.Value.ConnectionString);
+            var mDb = mClient.GetDatabase(dbSettings.Value.DatabaseName);
+            _contactCollection = mDb.GetCollection<Contact>(dbSettings.Value.ContactsCollectionName);
+        }
+
+        public List<Contact> Get() => _contactCollection.Find(x=>true).ToList();
+
+        public void Create(Contact _c) {
+            _contactCollection.InsertOne(_c);
+        
+        }
+
+        
+
+    }
+}
